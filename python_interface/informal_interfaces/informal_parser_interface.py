daec1f58739d66fe117bd490e41ec8c925a5f380
Kemal Diri <kemaldiri@gmail.com>
2021-04-23 19:52:36 +0300

    Informal interfaces :cherry_blossom:

---
+++ python_interface/informal_interfaces/informal_parser_interface.py
@@ -0,0 +1,45 @@
+
+
+class InformalParserInterface:
+    """
+    Informal parser interface
+    """
+    def load_data_source(self, path: str, file_name: str) -> str:
+        """
+        Load from file to load the text
+        :param path: file path
+        :param file_name: file name
+        :return: str
+        """
+        pass
+
+    def extract_text(self, full_file_name: str) -> dict:
+        """
+        Extract the text from the currently loaded file
+        :param full_file_name:
+        :return: dict
+        """
+        pass
+
+
+class PdfParser(InformalParserInterface):
+    """Extract the text from a pdf"""
+    def load_data_source(self, path: str, file_name: str) -> str:
+        """Overrides InformalParserInterface.load_data_source()"""
+        pass
+
+    def extract_text(self, full_file_name: str) -> dict:
+        """Overrides InformalParserInterface.extract_text()"""
+        pass
+
+
+class EmailParser(InformalParserInterface):
+    """Extract the text from a pdf"""
+    def load_data_source(self, path: str, file_name: str) -> str:
+        """Overrides InformalParserInterface.load_data_source()"""
+        pass
+
+    def extract_text_from_email(self, full_file_name: str) -> dict:
+        """A method defined in only EmailParser.
+        Doesn't overrides InformalParserInterface.extract_text()"""
+        pass
